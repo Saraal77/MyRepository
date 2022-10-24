6d7411237b2a2205d0b335ffa376e834aa6a77b8
Saraal77 <112335693+Saraal77@users.noreply.github.com>
2022-10-24 16:01:01 +0300

    Update jjj.java

--- jjj.java
+++ jjj.java
@@ -2,5 +2,6 @@ public class jjj {
     public static void main(String[] args) {
 
         System.out.println("hfhgfhg");
+        //////////////////////////////////////////////////////////////////////////////////////////
     }
 }
